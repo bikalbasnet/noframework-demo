52f08d8fddcc8216377a7dbc794ede13b8a03f21
Bikal <bikalbasnet775@gmail.com>
2018-04-11 15:08:05 +0700

    add error handler

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -1,3 +1,24 @@
 <?php declare(strict_types = 1);
 
-echo 'Hello World!';
+namespace Example;
+
+use \Whoops\Run;
+use \Whoops\Handler\PrettyPageHandler;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+error_reporting(E_ALL);
+
+$environment = 'development';
+
+$whoops = new Run;
+
+if ($environment !== 'production') {
+    $whoops->pushHandler(new PrettyPageHandler);
+} else {
+    $whoops->pushHandler(function($e) {
+        echo 'TODO';
+    });
+}
+
+$whoops->register();
